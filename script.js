850d1b16d9348903974a16dbde5f3d909fd0011f
Chi  Chan <chichan@Chis-MBP.lan>
2023-04-01 11:48:22 +0800

    connected the front-end with the back-end by having facts appear from the database

--- script.js
+++ script.js
@@ -1,6 +1,107 @@
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+  {
+    id: 4,
+    text: "JONAS",
+  },
+];
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+// Selecting DOM elements
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
+const factsList = document.querySelector(".facts-list");
 
+// Create DOM elements: Render facts in list
+factsList.innerHTML = "";
+
+// Load data from Supabase
+loadFacts();
+
+async function loadFacts() {
+  const res = await fetch(
+    "https://duyyufcpscvfukyuvcrm.supabase.co/rest/v1/facts",
+    {
+      headers: {
+        apiKey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1eXl1ZmNwc2N2ZnVreXV2Y3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk5ODYxMzksImV4cCI6MTk5NTU2MjEzOX0.YgkmrVXGo6O1MIUCyUIhVsnqMrVgYjftAqqHFPe-b3U",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1eXl1ZmNwc2N2ZnVreXV2Y3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk5ODYxMzksImV4cCI6MTk5NTU2MjEzOX0.YgkmrVXGo6O1MIUCyUIhVsnqMrVgYjftAqqHFPe-b3U",
+      },
+    }
+  );
+  const data = await res.json();
+  createFactsList(data);
+}
+
+// createFactsList(initialFacts);
+
+function createFactsList(dataArray) {
+  const htmlArray = dataArray.map(
+    (fact) => `<li class="fact">
+    <p>
+      ${fact.text}
+      <a 
+        class="source"
+        href="${fact.source}"
+        target="_blank"
+        >(Source)</a>
+    </p>
+    <span class="tag" style="background-color: #3b82f6"
+      >${fact.category}</span>
+    <div class="vote-buttons">
+      <button>👍 24</button>
+      <button>🤯 9</button>
+      <button>⛔️ 4</button>
+    </div>
+  </li>`
+  );
+
+  const html = htmlArray.join("");
+  factsList.insertAdjacentHTML("afterbegin", html);
+}
+
+// Toggle form visibility
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");

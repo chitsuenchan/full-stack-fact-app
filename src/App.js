3b422d5a21aa4dc63497afbfc713b6468e665730
chi chan <chitsuenchan@gmail.com>
2023-04-04 02:43:23 +0800

    added initial components and style sheet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,74 @@
+import "./style.css";
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+  {
+    id: 4,
+    text: "JONAS",
+  },
+];
+
 function App() {
-  return `<h1> Hello world</h1>`;
+  return (
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="Today I Learned Logo" />
+          <h1>Today I Learned</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
+  );
+}
+
+function NewFactForm() {
+  return <form>Fact Form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>HELLO THERE</aside>;
+}
+
+function FactList() {
+  return <form className="fact-list"></form>;
 }
 
 export default App;

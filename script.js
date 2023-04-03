fb3640e2f3eaf987c1f473b7b55912e2d75ff9fe
chi chan <chitsuenchan@gmail.com>
2023-04-04 00:05:52 +0800

    added dynamic colour mapping of category tags through filter function

--- script.js
+++ script.js
@@ -1,41 +1,3 @@
-const initialFacts = [
-  {
-    id: 1,
-    text: "React is being developed by Meta (formerly facebook)",
-    source: "https://opensource.fb.com/",
-    category: "technology",
-    votesInteresting: 24,
-    votesMindblowing: 9,
-    votesFalse: 4,
-    createdIn: 2021,
-  },
-  {
-    id: 2,
-    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
-    source:
-      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
-    category: "society",
-    votesInteresting: 11,
-    votesMindblowing: 2,
-    votesFalse: 0,
-    createdIn: 2019,
-  },
-  {
-    id: 3,
-    text: "Lisbon is the capital of Portugal",
-    source: "https://en.wikipedia.org/wiki/Lisbon",
-    category: "society",
-    votesInteresting: 8,
-    votesMindblowing: 3,
-    votesFalse: 1,
-    createdIn: 2015,
-  },
-  {
-    id: 4,
-    text: "JONAS",
-  },
-];
-
 const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
   { name: "science", color: "#16a34a" },
@@ -71,6 +33,7 @@ async function loadFacts() {
     }
   );
   const data = await res.json();
+
   createFactsList(data);
 }
 
@@ -87,8 +50,10 @@ function createFactsList(dataArray) {
         target="_blank"
         >(Source)</a>
     </p>
-    <span class="tag" style="background-color: #3b82f6"
-      >${fact.category}</span>
+    <span class="tag" style="background-color: ${
+      CATEGORIES.find((cat) => cat.name === fact.category)?.color
+    }">${fact.category}</span>
+
     <div class="vote-buttons">
       <button>👍 24</button>
       <button>🤯 9</button>

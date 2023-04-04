ed6081c396742f5af8c264ea59e6f8cee4be3bff
chi chan <chitsuenchan@gmail.com>
2023-04-04 17:56:53 +0800

    added fact component into factlist component and added max width of the page

--- src/App.js
+++ src/App.js
@@ -38,6 +38,17 @@ const initialFacts = [
   },
 ];
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 function App() {
   return (
     <>
@@ -60,15 +71,53 @@ function App() {
 }
 
 function NewFactForm() {
-  return <form>Fact Form</form>;
+  return <form className="fact-form">Fact Form</form>;
 }
 
 function CategoryFilter() {
-  return <aside>HELLO THERE</aside>;
+  return <aside>CATEGORIES</aside>;
 }
 
 function FactList() {
-  return <form className="fact-list"></form>;
+  // Temporary facts
+  const facts = initialFacts;
+
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <Fact key={fact.id} fact={fact} />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+function Fact({ fact }) {
+  return (
+    <li className="fact">
+      <p>
+        {fact.text}
+        <a className="source" href="https://opensource.fb.com/" target="_blank">
+          (Source)
+        </a>
+      </p>
+      <span
+        className="tag"
+        style={{
+          backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category)
+            ?.color,
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className="vote-buttons">
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔️ {fact.votesFalse}</button>
+      </div>
+    </li>
+  );
 }
 
 export default App;

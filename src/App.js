420ca1aae18051b33f3a61bde166aa5c9d9f3261
chi chan <chitsuenchan@gmail.com>
2023-04-05 16:15:59 +0800

    enhanced form. added 200 dynamic character length, prevent browser reloading on form submission, and ability to add a new fact to the fact list

--- src/App.js
+++ src/App.js
@@ -57,17 +57,18 @@ function Counter() {
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [facts, setFacts] = useState(initialFacts);
 
   return (
     <>
       {/* HEADER */}
       <Header showForm={showForm} setShowForm={setShowForm} />
 
-      {showForm ? <NewFactForm /> : null}
+      {showForm ? <NewFactForm setFacts={setFacts} /> : null}
 
       <main className="main">
         <CategoryFilter />
-        <FactList />
+        <FactList facts={facts} />
       </main>
     </>
   );
@@ -90,10 +91,6 @@ function Header({ showForm, setShowForm }) {
   );
 }
 
-function NewFactForm() {
-  return <form className="fact-form">Fact Form</form>;
-}
-
 const CATEGORIES = [
   { name: "technology", color: "#3b82f6" },
   { name: "science", color: "#16a34a" },
@@ -104,6 +101,81 @@ const CATEGORIES = [
   { name: "history", color: "#f97316" },
   { name: "news", color: "#8b5cf6" },
 ];
+
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+function NewFactForm({ facts, setFacts }) {
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("http://example.com");
+  const [category, setCategory] = useState("science");
+  const textLength = text.length;
+
+  function handleSubmit(e) {
+    // 1. Prevent the browser from reloading
+    e.preventDefault();
+
+    // 2. Check if the data is invalid. If so then create a new fact
+
+    if (text && isValidHttpUrl(source) && category && textLength) {
+      // 3. Create a new fact object
+
+      const newFact = {
+        id: Math.round(Math.random() * 10000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),
+      };
+
+      // 4. Add a new fact to the UI
+      setFacts((facts) => [newFact, ...facts]);
+    }
+
+    // 5. Reset input fields
+
+    // 6. Close the form
+
+    console.log(text, source, category);
+  }
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        type="text"
+        placeholder="Trustworthy source..."
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Choose Category</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
+}
 
 function CategoryFilter() {
   return (
@@ -129,10 +201,7 @@ function CategoryFilter() {
   );
 }
 
-function FactList() {
-  // Temporary facts
-  const facts = initialFacts;
-
+function FactList({ facts }) {
   return (
     <section>
       <ul className="facts-list">

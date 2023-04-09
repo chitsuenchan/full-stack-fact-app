41a35004597006eb71e3bfef9d31b99e305af138
chi chan <chitsuenchan@gmail.com>
2023-04-09 14:08:19 +0700

    added a loading component

--- src/App.js
+++ src/App.js
@@ -59,11 +59,14 @@ function Counter() {
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [facts, setFacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function getFacts() {
+      setIsLoading(true);
       const { data: facts, error } = await supabase.from("facts").select("*");
       setFacts(facts);
+      setIsLoading(false);
     }
 
     getFacts();
@@ -80,10 +83,14 @@ function App() {
 
       <main className="main">
         <CategoryFilter />
-        <FactList facts={facts} />
+        {isLoading ? <Loader /> : <FactList facts={facts} />}
       </main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="message">Loading...</p>;
 }
 
 function Header({ showForm, setShowForm }) {
